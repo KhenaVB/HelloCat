1d0ac5281dc3d6c73f60038ae45e9006235d3a96
khenavb <khenavb@gmail.com>
2023-04-26 21:35:07 +0200

    lol

--- HelloCat/Program.cs
+++ HelloCat/Program.cs
@@ -1,24 +1,24 @@
 ﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        List<Question> questions = new List<Question>();
-        questions.Add(new Question("question 1", "hoe oud is moesie in katten jaren?", "80"));
-        questions.Add(new Question("question 2", "welk soort kat is moesie?", "cymric"));
+
+    private static void Main(string[] args
+   
+        List<Question> questions = new List<Question>()
+        questions.Add(new Question("question 1", "hoe oud is moesie in katten jaren?", "80")
+        questions.Add(new Question("question 2", "welk soort kat is moesie?", "cymric")
         int correct = 0;
         foreach (Question question in questions)
         {
-            if (question.Ask())
+            if (question.Ask()
             {
-                correct++;
+                correct++
             }
-            Console.ReadLine();
-            Console.Clear();
-        }
+            Console.ReadLine()
+            Console.Clear()
         
         
         
-        Console.WriteLine(string.Format("{0} / {1}", correct, 2));
+        
+        Console.WriteLine(string.Format("{0} / {1}", correct, 2)
     }
 
     
@@ -27,16 +27,16 @@
 
 internal class Question
 {
-    public string title, message, correct_antwoord;
+    public string title, message, correct_antwoord
     public Question(string title, string message, string correct_antwoord)
     {
         this.title = title;
         this.message = message;
-        this.correct_antwoord = correct_antwoord;
+        this.correct_anwoord = correct_antwoord
     }
     public bool Ask()
     {
-        Console.WriteLine(title);
+        Console.WriteLne(title);
         Console.WriteLine(message);
 
         if (Console.ReadKey().Equals(null))
@@ -53,8 +53,8 @@ internal class Question
         }
         else
         {
-            Console.WriteLine("incorrect");
-            return false;
+            console.WriteLine("incorrect");
+            reurn false;
         }
     }
 }
